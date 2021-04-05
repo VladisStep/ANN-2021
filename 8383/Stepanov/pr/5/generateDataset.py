c06449fafeb3542a5a121d5d319a4682a779180d
Vladislav <vlstep2200@gmail.com>
2021-04-05 22:28:23 +0300

    add other methods

--- 8383/Stepanov/pr/5/generateDataset.py
+++ 8383/Stepanov/pr/5/generateDataset.py
@@ -3,13 +3,25 @@ import numpy as np
 x = np.random.normal(3.0, 10.0, 3000).reshape(-1,1) # генерируем х
 e = np.random.normal(0, 0.3, 3000).reshape(-1,1)  # генерируем ошибку
 
-y = np.sin(x/2) + e # считаем y
+y_1 = x**2 + e
+y_2 = np.sin(x/2) + e
+y_3 = np.cos(2*x) + e
+y_4 = x - 3 + e
+y_5 = -x + e
+y_6 = abs(x) + e
+y_7 = (x**3)/4 + e
 
-np.savetxt('train_dataset.csv', np.hstack((x, y))) # записываем в файл
+np.savetxt('train_dataset.csv', np.hstack((y_1, y_3, y_4, y_5, y_6, y_7, y_2))) # записываем в файл
 
 x = np.random.normal(3.0, 10.0, 1000).reshape(-1,1)
 e = np.random.normal(0, 0.3, 1000).reshape(-1,1)
 
-y = np.sin(x/2) + e
+y_1 = x**2 + e
+y_2 = np.sin(x/2) + e
+y_3 = np.cos(2*x) + e
+y_4 = x - 3 + e
+y_5 = -x + e
+y_6 = abs(x) + e
+y_7 = (x**3)/4 + e
 
-np.savetxt('test_dataset.csv', np.hstack((x, y)))
+np.savetxt('test_dataset.csv', np.hstack((y_1, y_3, y_4, y_5, y_6, y_7, y_2)))
--- 8383/Stepanov/pr/5/main.py
+++ 8383/Stepanov/pr/5/main.py
@@ -1,41 +1,42 @@
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
+from keras.layers import Input, Dense
 
-train_dataset = np.fromfile('train_dataset.csv', dtype ='float', sep =' ').reshape(-1, 2)
-test_dataset = np.fromfile('test_dataset.csv', dtype ='float', sep =' ').reshape(-1, 2)
+train_dataset = np.fromfile('train_dataset.csv', dtype ='float', sep =' ').reshape(-1, 7)
+test_dataset = np.fromfile('test_dataset.csv', dtype ='float', sep =' ').reshape(-1, 7)
 
-train_data = train_dataset[:, 0].reshape(-1,1)
-train_targets = train_dataset[:, 1].reshape(-1,1)
+train_data = train_dataset[:, 0:6].reshape(-1, 6)
+train_targets = train_dataset[:, 6].reshape(-1, 1)
 
-test_data = test_dataset[:, 0].reshape(-1,1)
-test_targets = test_dataset[:, 1].reshape(-1,1)
+test_data = test_dataset[:, 0:6].reshape(-1, 6)
+test_targets = test_dataset[:, 6].reshape(-1, 1)
 
-CONST_FOR_ENCODE = 5.0
-
-inp = Input(shape=(1,))
+inp = Input(shape=(6,))
 
 # encoder
-dense_ecoder = Dense(1, activation='relu', name='dense_ecoder')(inp)
+dense_ecoder = Dense(64, activation='relu', name='dense_ecoder')(inp)
+dense_ecoder = Dense(32, activation='relu')(dense_ecoder)
+dense_ecoder = Dense(4, activation='relu')(dense_ecoder)
 
 # regression
-dense_1 = Dense(64, activation='relu')(dense_ecoder)
+dense_1 = Dense(4, activation='relu')(dense_ecoder)
 dense_2 = Dense(64, activation='relu')(dense_1)
 out_reg = Dense(1, name='out_reg')(dense_2)
 
 # decoder
-dense_decoder = Dense(1, name='dense_decoder')(dense_ecoder)
+dense_decoder = Dense(32, activation='relu', name='dense_decoder_1')(dense_ecoder)
+dense_decoder = Dense(64, activation='relu', name='dense_decoder_2')(dense_decoder)
+dense_decoder = Dense(6, activation='relu', name='dense_decoder_out')(dense_decoder)
 
 # configuration
-model = Model(inputs=inp, outputs=[out_reg, dense_decoder, dense_ecoder])
+model = Model(inputs=inp, outputs=[out_reg, dense_decoder])
 model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
-model.fit(train_data, [train_targets, train_data, train_data * CONST_FOR_ENCODE], epochs=200, batch_size=10)
-
+model.fit(train_data, [train_targets, train_data], epochs=200, batch_size=10)
 
 # encoder model
 ecoder_model = Model(inputs=inp, outputs=dense_ecoder)
 encoder_pred = np.asarray(ecoder_model.predict(test_data))
-np.savetxt('outOfModels/data_encoder_model.csv', np.hstack((test_data * CONST_FOR_ENCODE, encoder_pred)))
+np.savetxt('outOfModels/data_encoder_model.csv', encoder_pred)
 ecoder_model.save('models/encoder_model.h5')
 
 # regression model
@@ -45,10 +46,9 @@ np.savetxt('outOfModels/data_reg_model.csv', np.hstack((test_targets, reg_pred))
 ecoder_model.save('models/reg_model.h5')
 
 # decoder model
-decoder_model = Model(inputs=inp, outputs=dense_decoder)
-decoder_pred = np.asarray(reg_model.predict(test_data * CONST_FOR_ENCODE))
-np.savetxt('outOfModels/data_decoder_model.csv', np.hstack((test_data, decoder_pred)))
+decoder = Model(inp, dense_decoder)
+decoder_pred = np.asarray(decoder.predict(test_data))
+np.savetxt('outOfModels/data_decoder_model.csv', decoder_pred)
 ecoder_model.save('models/decoder_model.h5')
-
 
 print('end')
